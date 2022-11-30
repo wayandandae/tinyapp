89f7b3850388c3ab83c893b2df9ff8e92b3193b0
Jack Kang <jkang0724@hotmail.com>
2022-11-30 13:04:03 -0500

    Add ejs templates to create a homepage with shortened URLs

--- express_server.js
+++ express_server.js
@@ -21,6 +21,12 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = { greeting: "Hello World!" };
+  res.render("hello_world", templateVars);
 });
